57c047a1133f9d0d35dbb3d795249114d91e9710
Leonard Moses <qalexeon@Polaris.lan>
2022-08-31 16:02:55 -0400

    Basic test layout drated

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,11 @@
 import React from "react";
 import { BrowserRouter, Route, Link } from "react-router-dom";
+import "../styles/Navbar.scss"
 
 const Navbar = () => {
     return ( 
-        <nav>
+        <nav className="Nav">
+            <div className="Logo"></div>
             <ul>
                 <li><Link to="/">Home</Link></li>
                 <li><Link to="/about">About</Link></li>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,20 @@
 import React from "react";
+import "../styles/Home.scss"
 
 
 
 const Home = () => {
     return ( 
-        <div>
-            <h1>Home Page</h1>
-            <p>Tempor incididunt Lorem aliquip deserunt magna velit. Sint cillum proident velit aute esse excepteur consequat dolor mollit. Commodo Lorem fugiat duis pariatur et labore magna exercitation. Duis laboris veniam irure elit.</p>
+        <div className="Home">
+            <h2>Heading2</h2>
+
+            <div className="HomeContent">
+                <h3>Heading3</h3>
+
+                <p>Tempor incididunt Lorem aliquip deserunt magna velit. Sint cillum proident velit aute esse excepteur consequat dolor mollit. Commodo Lorem fugiat duis pariatur et labore magna exercitation. Duis laboris veniam irure elit.</p>
+
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vel hic, qui impedit ipsum, rerum eum fuga laboriosam iusto minus aperiam nihil doloribus dolore dignissimos voluptatum vitae voluptas et. Eos, doloremque.</p>
+            </div>
         </div>
 
      );
