749386d7d4131524d936f0d2f75e173b0589e132
Qalexeon <qalexeon@Xavier.lan>
2022-09-26 16:06:07 -0400

    update

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -14,6 +14,8 @@ const Home = () => {
                 <p>Tempor incididunt Lorem aliquip deserunt magna velit. Sint cillum proident velit aute esse excepteur consequat dolor mollit. Commodo Lorem fugiat duis pariatur et labore magna exercitation. Duis laboris veniam irure elit.</p>
 
                 <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vel hic, qui impedit ipsum, rerum eum fuga laboriosam iusto minus aperiam nihil doloribus dolore dignissimos voluptatum vitae voluptas et. Eos, doloremque.</p>
+                
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vel hic, qui impedit ipsum, rerum eum fuga laboriosam iusto minus aperiam nihil doloribus dolore dignissimos voluptatum vitae voluptas et. Eos, doloremque.</p>
             </div>
         </div>
 
